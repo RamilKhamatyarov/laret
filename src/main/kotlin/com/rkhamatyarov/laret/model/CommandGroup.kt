d0babc4329a66d415da164d3a740d26df569ce34
RamilKhamatyarov <18293934+RamilKhamatyarov@users.noreply.github.com>
2026-01-27 20:03:04 +0300

    feature (help formatter refactoring): (#38)
    - Extract showHelp() from CommandGroup, Command, CliApp
    - Create HelpFormatter as single source of truth
    - Reduce code duplication (~100 lines)
    - Improve Single Responsibility Principle
    
    Co-authored-by: ramil-khamatyarov <ramil.hamatyarov@yandex.ru>

--- src/main/kotlin/com/rkhamatyarov/laret/model/CommandGroup.kt
+++ src/main/kotlin/com/rkhamatyarov/laret/model/CommandGroup.kt
@@ -1,7 +1,5 @@
 package com.rkhamatyarov.laret.model
 
-import org.slf4j.LoggerFactory
-
 /**
  * Represents a group of related commands
  */
@@ -9,14 +7,4 @@ data class CommandGroup(
     val name: String,
     val description: String = "",
     val commands: List<Command> = emptyList(),
-) {
-    private val log = LoggerFactory.getLogger(javaClass::class.java)
-
-    fun showHelp() {
-        log.info("Group: $name - $description")
-        log.info("\nCommands:")
-        commands.forEach { cmd ->
-            log.info("  ${cmd.name.padEnd(20)} ${cmd.description}")
-        }
-    }
-}
+)
